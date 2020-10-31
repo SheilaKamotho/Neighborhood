3ace89e499f83ffd450d433562fc0a306fe48acd
Kamotho Sheila <kamothosheila@gmail.com>
2020-10-31 04:34:29 +0300

    create profile table

--- hood/models.py
+++ hood/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class NeighborHood(models.Model):
@@ -19,3 +20,20 @@ class NeighborHood(models.Model):
         hoods=cls.objects.filter()
         return hoods
 
+class Profile(models.Model):
+    name = models.CharField(max_length = 60)
+    id_no = models.IntegerField()
+    hood_id = models.ForeignKey(NeighborHood, on_delete=models.CASCADE)
+    email = models.CharField(max_length = 60)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def save_profile(cls):
+        profile=cls.objects.filter()
+        return profile
