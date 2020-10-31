ba7e58fd78f628d8e1fec21ad61c02e2e86eeec9
Kamotho Sheila <kamothosheila@gmail.com>
2020-10-31 04:41:51 +0300

    register business and profile tables to admin

--- hood/admin.py
+++ hood/admin.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import NeighborHood
+from .models import NeighborHood, Profile, Business
 
 # Register your models here.
 admin.site.register(NeighborHood)
+admin.site.register(Profile)
+admin.site.register(Business)
