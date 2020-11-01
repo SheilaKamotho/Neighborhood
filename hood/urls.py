a428e6055c7e601d27dedf4711567d0871b08f27
Kamotho Sheila <kamothosheila@gmail.com>
2020-11-01 21:17:42 +0300

    add search claamethod

--- hood/urls.py
+++ hood/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.home,name = 'home'),
+    url('^post/$',views.new_post,name = 'post'),
+    url('^business/$',views.business,name = 'business'),
     
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- hood/views.py
+++ hood/views.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Profile, Post, Business
+from .forms import NewPostForm
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def home(request):
-    return render(request, 'home/home.html')
+    profile=Profile.objects.all()
+    posts=Post.objects.all()
+    return render(request, 'home/home.html',{"profile":profile, "posts":posts})
+
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+        return redirect('home')
+        
+    else:
+        form = NewPostForm()
+    return render(request, 'home/new_posts.html', {"form":form, "current_user":current_user})
+
+@login_required(login_url='/accounts/login/')
+def business(request):
+    businesses=Business.objects.all()
+    return render(request, 'home/business.html',{"businesses":businesses})
